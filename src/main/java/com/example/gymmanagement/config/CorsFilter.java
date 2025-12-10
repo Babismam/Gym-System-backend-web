870055eb3d05acba15b6ed49f4864b43f3a89656
Babismam <babis1993@gmail.com>
2025-12-10 19:03:42 +0200

    Backend Web 10/12/25

--- src/main/java/com/example/gymmanagement/config/CorsFilter.java
+++ src/main/java/com/example/gymmanagement/config/CorsFilter.java
@@ -16,14 +16,20 @@ public class CorsFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+        res.setHeader("Access-Control-Allow-Origin", "https://gym-system-app-frontend.netlify.app");
+
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+
         res.setHeader("Access-Control-Allow-Credentials", "true");
+
 
         if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
             res.setStatus(HttpServletResponse.SC_OK);
         } else {
+
             chain.doFilter(request, response);
         }
     }
