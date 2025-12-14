8f359fba4fba5fb2ea7bc82b85d7c1e5e76d7df0
Babismam <babis1993@gmail.com>
2025-12-14 11:16:47 +0200

    Backend Web 14/12/25

--- src/main/java/com/example/gymmanagement/api/ApplicationConfig.java
+++ src/main/java/com/example/gymmanagement/api/ApplicationConfig.java
@@ -1,8 +1,28 @@
 package com.example.gymmanagement.api;
 
+import com.example.gymmanagement.resources.*;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ApplicationPath("/api")
 public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+
+        classes.add(AttendanceResource.class);
+        classes.add(AuthResource.class);
+        classes.add(DashboardResource.class);
+        classes.add(OpeningHoursResource.class);
+        classes.add(ProgramResource.class);
+        classes.add(ScheduleResource.class);
+        classes.add(TrainerResource.class);
+        classes.add(UserResource.class);
+
+        return classes;
+    }
 }
